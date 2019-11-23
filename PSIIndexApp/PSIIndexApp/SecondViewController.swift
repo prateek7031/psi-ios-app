812d6719ecb5509dad2bd6804679103887625f6e
Prateek <prateek@Prateeks-MacBook-Pro-1822.local>
2019-11-23 11:20:18 +0800

    Initial setup

---
+++ PSIIndexApp/PSIIndexApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PSIIndexApp
+//
+//  Created by Prateek on 23/11/19.
+//  Copyright © 2019 Prateek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PSIIndexApp/Pods/Target Support Files/Pods-PSIIndexAppTests/Pods-PSIIndexAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PSIIndexAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_PSIIndexAppTests
+@end
